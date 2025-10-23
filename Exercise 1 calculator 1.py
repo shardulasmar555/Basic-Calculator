9e6a7b2a116c3b7857769e125ee7670fed6e8988
shardulasmar555 <shardulasmar555@gmail.com>
2025-10-23 18:44:55 +0200

    Add files via upload

---
+++ Exercise 1 calculator 1.py
@@ -0,0 +1,15 @@
+# Create a calculator
+
+a=15
+b=7
+
+print("The value of",a,"+",b,"is:",a+b)
+print("The value of",a,"-",b,"is:",a-b)
+print("The value of",a,"*",b,"is:",a*b)
+print("The value of",a,"/",b,"is:",a/b)
+print("The value of",a,"//",b,"is:",a//b)
+print("The value of",a,"%",b,"is:",a%b)
+print("The value of",a,"**",b,"is:",a**b)
+
+
+
