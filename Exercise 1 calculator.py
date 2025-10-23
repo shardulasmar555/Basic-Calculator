d8f0f15ac6f4bdd6882c69a1d1263e222a16dc9a
shardulasmar555 <shardulasmar555@gmail.com>
2025-10-23 18:33:16 +0200

    Add files via upload

---
+++ Exercise 1 calculator.py
@@ -0,0 +1,19 @@
+# Create a calculator
+
+a=15
+b=7
+ans1=a+b
+print(ans1) #addition of a and b
+ans2=a-b
+print(ans2) #substraction of a and b
+ans3=a*b
+print(ans3)  #multiplication of a and b
+ans4=a**b
+print(ans4)
+ans5=a//b
+print(ans5)  #floor division of a and b
+ans6=a%b
+print(ans6)  # module of a and b
+ans7=a/b
+print(ans7)  # division of a and b
+
